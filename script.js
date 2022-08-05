49ab29b0f0eda6415bbdb66197c64757c6df7044
Carlos Santos <0102522@pt.softinsa.com>
2022-08-05 18:07:41 +0100

    fixed painting divs

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-//criar div 2x2 
-//criar div 16x16 
-//pintar as borders das divs 
-// your JavaScript file
-
 const container = document.querySelector('.container');
-for(let i = 1 ; i<=256; i++){
-    container.innerHTML+='<div class = "element" onmouseover="mouseOverEvent()")></div>\n';
+for(let i = 0 ; i<256; i++){
+    container.innerHTML+='<div class = "element" onmouseover="mouseOverEvent()"></div>\n';
 }
 
-const element = document.querySelector('.element');
+
 function mouseOverEvent(){
-    console.log(element.style.background);
-    element.setAttribute('style', 'background: yellow;');  
+    const myall = Array.from(document.querySelectorAll('.element'));
+    const elements = document.querySelectorAll('.element');
+    document.addEventListener('mouseover', (e) => {
+       if(e.target.classList.contains('element')){
+          elements[myall.indexOf(e.target)].style.background="red"; 
+        }
+    });
 }
