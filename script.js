46fd7b3b25c4e9f4c64bf1794aaaac677d01f13c
Carlos <carlosmsantos95@gmail.com>
2022-08-06 01:26:25 +0100

    Added Clear button

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const container = document.querySelector('.container');
-for(let i = 0 ; i<256; i++){
-    container.innerHTML+='<div class = "element" onmouseover="mouseOverEvent()"></div>\n';
-}
-
-
 function mouseOverEvent(){
     const myall = Array.from(document.querySelectorAll('.element'));
     const elements = document.querySelectorAll('.element');
@@ -13,3 +7,31 @@ function mouseOverEvent(){
         }
     });
 }
+
+function mouseClickEvent(){
+    const elementsArray = Array.from(document.querySelectorAll('.element'));
+    const elements = document.querySelectorAll('.element');
+    console.log(elementsArray.length);
+    for(let i = 0; i<elementsArray.length;i++){
+    elements[i].style.background ="white";
+    }
+}
+
+//creates grid
+const container = document.querySelector('.container');
+for(let i = 0 ; i<256; i++){
+    container.innerHTML+='<div class = "element" onmouseover="mouseOverEvent()"></div>\n';
+}
+
+const buttonContainer = document.createElement('div');
+buttonContainer.setAttribute('id', 'button-container');
+const button = document.createElement('button');
+button.setAttribute('id', 'clear-button');
+button.setAttribute('onclick', 'mouseClickEvent()');
+button.innerHTML="Clear!";
+buttonContainer.append(button)
+container.insertAdjacentElement('afterend', buttonContainer);
+
+//beforebegin
+
+//investigar a situaçao de inserir uma variavel no css 
