78107e45e7c7feea16e4abd248e1bff97b3d0f83
Carlos <carlosmsantos95@gmail.com>
2022-08-08 15:36:18 +0100

    Finished Project :)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const container = document.querySelector('.container');
-const initialColor = "red";
+const grid = document.querySelector('.grid');
+const initialColor = "gray";
 var randomColor = "#"+Math.floor(Math.random()*16777215).toString(16);
 const containerFlex = document.querySelector('.container-flex');
 let pressedColorButton = 0;
@@ -8,14 +8,13 @@ let pressedGridButton = 0;
 createGrid(32);
 
 function createGrid(gridSize){
-    let divSize = container.clientWidth / gridSize;
+    let divSize = grid.clientWidth / gridSize;
     for (let i = 0; i < gridSize*gridSize; i++) {
-            container.innerHTML+=`<div class = "element" onmouseover="mouseOverEvent('${initialColor}')"></div>\n`;
+            grid.innerHTML+=`<div class = "element" onmouseover="mouseOverEvent('${initialColor}')"></div>\n`;
             createDiv(divSize);
     }
-    container.setAttribute('style', `grid-template-rows: repeat(${gridSize}, 1fr);grid-template-columns: repeat(${gridSize}, 1fr);`);
+    grid.setAttribute('style', `grid-template-rows: repeat(${gridSize}, 1fr);grid-template-columns: repeat(${gridSize}, 1fr);`);
 }
-
 
 const elementsArray = Array.from(document.querySelectorAll('.element'));
 const elements = document.querySelectorAll('.element');
@@ -27,8 +26,6 @@ function createDiv(size) {
         elements[i].style.height = `${size}px`;
     }
   }
-
-  
 
 function mouseOverEvent(color){
     document.addEventListener('mouseover', (e) => {
@@ -76,44 +73,15 @@ function gridButtonMouseClickEvent(){
     }
 }
 
-
-
-/*****************
- * HTML ELEMENTS *
- ****************/
-
-//creates clear button
-const clearButtonContainer = document.createElement('div');
-clearButtonContainer.setAttribute('id', 'clear-button-container');
-const clearButton = document.createElement('button');
-clearButton.setAttribute('id', 'clear-button');
-clearButton.setAttribute('onclick', 'clearButtonMouseClickEvent()');
-clearButton.innerHTML="Clear!";
-clearButtonContainer.append(clearButton)
-container.insertAdjacentElement('afterend', clearButtonContainer);
-
-
-//creates toggle grid button
-const selectClearButtonContainer = document.querySelector('#clear-button-container');//selects the clearButton container
-const gridButtonContainer = document.createElement('div');
-gridButtonContainer.setAttribute('id', 'grid-button-container');
-const gridButton = document.createElement('button');
-gridButton.setAttribute('id', 'grid-button');
-gridButton.setAttribute('onclick', 'gridButtonMouseClickEvent()');
-gridButton.innerHTML="Toggle Grid";
-gridButtonContainer.append(gridButton)
-selectClearButtonContainer.insertAdjacentElement('afterend', gridButtonContainer);
-
 //creates random color button
-const selectGridButtonContainer = document.querySelector('#grid-button-container');//selects the gridButton container
-const colorButtonContainer = document.createElement('div');
-colorButtonContainer.setAttribute('id', 'color-button-container');
+const selectGridButton = document.querySelector('#grid-button');//selects the gridButton container
+console.log(selectGridButton);
 const colorButton = document.createElement('button');
 colorButton.setAttribute('id', 'color-button');
 colorButton.setAttribute('onclick', 'colorButtonMouseClickEvent()');
 colorButton.innerHTML="Random Color";
-colorButtonContainer.append(colorButton)
-selectGridButtonContainer.insertAdjacentElement('afterend', colorButtonContainer);
+selectGridButton.insertAdjacentElement('afterend', colorButton);
+
 
 //creates slider and it's values
 var slider = document.getElementById("slider");
@@ -123,7 +91,7 @@ function sliderValue(){
     if(slider.value === "1"){
         numberOfCells.innerHTML = "Small";
         clearButtonMouseClickEvent();
-        container.setAttribute('style', `grid-template-rows: repeat(4, 1fr);grid-template-columns: repeat(4, 1fr);`);
+        grid.setAttribute('style', `grid-template-rows: repeat(4, 1fr);grid-template-columns: repeat(4, 1fr);`);
         for(let i = 0; i<elementsArray.length;i++){
             elements[i].style.width = `100px`;
             elements[i].style.height = `100px`;
@@ -132,7 +100,7 @@ function sliderValue(){
     else if (slider.value === "2"){
         numberOfCells.innerHTML = "Medium";
         clearButtonMouseClickEvent();
-        container.setAttribute('style', `grid-template-rows: repeat(16, 1fr);grid-template-columns: repeat(16, 1fr);`);
+        grid.setAttribute('style', `grid-template-rows: repeat(16, 1fr);grid-template-columns: repeat(16, 1fr);`);
         for(let i = 0; i<elementsArray.length;i++){
             elements[i].style.width = `50px`;
             elements[i].style.height = `50px`;
@@ -141,7 +109,7 @@ function sliderValue(){
     else{
         clearButtonMouseClickEvent();
         numberOfCells.innerHTML = "Big";
-        container.setAttribute('style', `grid-template-rows: repeat(32, 1fr);grid-template-columns: repeat(32, 1fr);`);
+        grid.setAttribute('style', `grid-template-rows: repeat(32, 1fr);grid-template-columns: repeat(32, 1fr);`);
         for(let i = 0; i<elementsArray.length;i++){
             elements[i].style.width = `25px`;
             elements[i].style.height = `25px`;
